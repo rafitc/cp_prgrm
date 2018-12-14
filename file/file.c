511057230788d4c77f21706484fc84f358cdab98
rafitc <rafirasheed.tc99@gmail.com>
2018-12-14 21:22:18 +0530

    file program added , still have some error

--- file/file.c
+++ file/file.c
@@ -1,1 +1,36 @@
-#include<stdio.h>
+#include <conio.h>
+#include <stdio.h>
+void main(){
+    FILE *fp1,*fp2;
+//    int i,j;
+    char c,ch;
+    //clrscr();
+    fp1 = fopen("FileOne","w");
+    if (fp1 == NULL) {
+        printf("Cannot open file \n");
+}
+    printf("Enter charcters to save in file \n");
+    while((c=getchar()) != 'Z')
+        putc(c,fp1);
+    fclose(fp1);
+    //copy
+    fp1 = fopen("FileTwo","r");
+    fp2 = fopen("FileTwo","w");
+    if (fp1 == NULL) {
+        printf("Cannot open file \n");
+}
+    else if (fp2 == NULL) {
+        printf("Cannot open file \n");
+}
+    while (1) {
+        ch = fgetc(fp1);
+
+        if (ch == EOF)
+	       break;
+        else
+	       putc(ch, fp2);
+}
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+}

05bb7c7a619011b6658a3d26a4e43d5bb1894d6f
rafitc <rafirasheed.tc99@gmail.com>
2019-01-21 21:40:57 +0530

    added happy number

--- Exam/HappyNumber.c
+++ Exam/HappyNumber.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <conio.h>
+void happy(int);
 /*A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number either equals 1 (where it will stay), or it loops endlessly in a cycle that does not include 1.
 If it is not happy number it will end at '4'. */
 
 void main(){
-    int num,a,sum=0,number;
+    int num;
     printf("ENter a number ");
     scanf("%d",&num);
+    happy(num);
+}
+void happy(int num){
+    int a,sum=0,number;
     number = num;         //backup number
         while(num != 1 && num != 4){
             while(num != 0){
